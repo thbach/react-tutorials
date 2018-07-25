683397c7673c78275d5b451c9413246e7d4de3dc
tbach <thbach@gmail.com>
2018-07-25 00:41:25 -0700

    withclass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
     constructor(props) {
@@ -9,14 +10,14 @@ class Person extends Component {
 
       render () {
         return (
-        <div className = {classes.Person}>
+        <WithClass classes={classes.Person}>
             <p onClick={this.props.click}>I'm a {this.props.name} annd I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input 
                 type="text" 
                 onChange={this.props.changed} 
                 value={this.props.name}/>
-        </div>
+        </WithClass>
 
         )
     }
